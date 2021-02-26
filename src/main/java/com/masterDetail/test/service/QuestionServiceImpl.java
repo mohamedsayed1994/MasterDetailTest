69c37d3005a0beaada22d332d7e6d8ed1829cd90
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2021-02-26 20:26:47 +0200

    update QuestionRep name to QuestionRepo

--- src/main/java/com/masterDetail/test/service/QuestionServiceImpl.java
+++ src/main/java/com/masterDetail/test/service/QuestionServiceImpl.java
@@ -1,7 +1,7 @@
 package com.masterDetail.test.service;
 
 import com.masterDetail.test.entity.Question;
-import com.masterDetail.test.repository.QuestionRep;
+import com.masterDetail.test.repository.QuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,30 +9,30 @@ import java.util.List;
 
 @Service
 public class QuestionServiceImpl implements QuestionService{
-    private QuestionRep questionRep;
+    private QuestionRepo questionRepo;
 
     @Autowired
-    public QuestionServiceImpl(QuestionRep questionRep) {
-        this.questionRep = questionRep;
+    public QuestionServiceImpl(QuestionRepo questionRepo) {
+        this.questionRepo = questionRepo;
     }
 
     @Override
     public List<Question> findAll() {
-        return this.questionRep.findAll();
+        return this.questionRepo.findAll();
     }
 
     @Override
     public Question findById(Long id) {
-        return this.questionRep.findById(id).orElse(null);
+        return this.questionRepo.findById(id).orElse(null);
     }
 
     @Override
     public Question create(Question question) {
-        return this.questionRep.save(question);
+        return this.questionRepo.save(question);
     }
 
     @Override
     public Question update(Question question) {
-        return this.questionRep.save(question);
+        return this.questionRepo.save(question);
     }
 }
